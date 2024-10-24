99ef6e7f4ad5e4b382f921d0bbd216b35a25df97
Subocol <edisonbermudez@subocol.com>
2024-10-24 16:57:48 -0500

    A

--- whatsapp.py
+++ whatsapp.py
@@ -73,7 +73,17 @@ st.html(
 st.table(st.session_state['df'])  # Mostrar la tabla
 num_columns = len(st.session_state['df'].columns)-1
 
-st.html(f"<p><span style=''>{num_columns}</span>!</p>")
+st.html(f"""
+<p>
+    <span style='font-family: "Roboto"; font-size: 14px; color: #025a7a; font-weight: 500;'>
+        Total Avisos: 
+    </span>
+    <span style='border-radius: 13px; height: 27px; width: auto; background-color: #ffb92b; color: #FFF; font-size: 16px; font-weight: 700;'>
+        {num_columns}
+    </span>
+</p>
+""")
+
 
 
 # Botón para recargar los datos
