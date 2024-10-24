7b0c231353512eea424ce5dd978fb4e67918cd17
Subocol <edisonbermudez@subocol.com>
2024-10-24 10:54:17 -0500

    A

--- whatsapp.py
+++ whatsapp.py
@@ -4,42 +4,7 @@ import pandas as pd
 import streamlit as st
 
 
-original_title = '<h1 style="font-family: serif; color:white; font-size: 20px;">Streamlit CSS Styling✨ </h1>'
-st.markdown(original_title, unsafe_allow_html=True)
-
-
-# Set the background image
-background_image = """
-<style>
-[data-testid="stAppViewContainer"] > .main {
-    background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
-    background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
-    background-position: center;  
-    background-repeat: no-repeat;
-}
-</style>
-"""
-
-st.markdown(background_image, unsafe_allow_html=True)
-
-st.text_input("", placeholder="Streamlit CSS ")
-
-input_style = """
-<style>
-input[type="text"] {
-    background-color: transparent;
-    color: #a19eae;  // This changes the text color inside the input box
-}
-div[data-baseweb="base-input"] {
-    background-color: transparent !important;
-}
-[data-testid="stAppViewContainer"] {
-    background-color: transparent !important;
-}
-</style>
-"""
-st.markdown(input_style, unsafe_allow_html=True)
-
+st.sidebar.image("https://i1.sndcdn.com/avatars-TUVYyVNGNRk1TF07-p27gng-t500x500.jpg", use_column_width=True)
 
 
 # Display the logo from a URL
