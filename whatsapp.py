480b0bce434427973f1b4c91ef3ed6b98f77f19e
Subocol <edisonbermudez@subocol.com>
2024-10-24 16:30:14 -0500

    Update url

--- whatsapp.py
+++ whatsapp.py
@@ -56,14 +56,16 @@ def load_data():
 
     # Reordenar las columnas según el nuevo nombre
     df = df.reindex(columns=["Placa", "Aviso", "Estado", "Fecha", "Nota"])
-    df.style.hide(axis="index").to_html()
+
     return df
 
 # Cargar los datos iniciales
 if 'df' not in st.session_state:
     st.session_state['df'] = load_data()  # Cargar datos al iniciar la aplicación
-    st.title("Observaciones")
-    st.table(st.session_state['df'])  # Mostrar la tabla
+
+st.markdown(df.style.hide(axis="index").to_html(), unsafe_allow_html=True)
+st.title("Observaciones")
+st.table(st.session_state['df'])  # Mostrar la tabla
 
 # Botón para recargar los datos
 if st.button("Recargar datos"):
